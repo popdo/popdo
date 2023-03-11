14073f134dff6af12b75d4d3bc991521b3672138
金 <30309735+popdo@users.noreply.github.com>
2023-03-11 23:05:12 +0800

    Create chat.ts

---
+++ src/pages/api/chat.ts
@@ -0,0 +1,56 @@
+import type { APIRoute } from 'astro'
+import { fetch, ProxyAgent } from 'undici'
+import { createParser, ParsedEvent, ReconnectInterval } from 'eventsource-parser'
+
+const httpsProxy = import.meta.env.HTTPS_PROXY
+const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
+
+export const post:APIRoute = async (context:any) => {
+  const options = await context.request.json()
+
+  if (httpsProxy) {
+    options['dispatcher'] = new ProxyAgent(httpsProxy)
+  }
+
+  const response = await fetch(`${baseUrl}/v1/chat/completions`, options) as Response
+
+  const encoder = new TextEncoder()
+  const decoder = new TextDecoder()
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const streamParser = (event: ParsedEvent | ReconnectInterval) => {
+        if (event.type === 'event') {
+          const data = event.data
+          if (data === '[DONE]') {
+            controller.close()
+            return
+          }
+          try {
+            const json = JSON.parse(data)
+            const text = json.choices[0].delta?.content
+            const queue = encoder.encode(text)
+            controller.enqueue(queue)
+          } catch (e) {
+            controller.error(e)
+          }
+        }
+      }
+
+      const parser = createParser(streamParser)
+      response.body.pipeTo(new WritableStream({
+        write(chunk) {
+          parser.feed(decoder.decode(chunk))
+        },
+        close() {
+          controller.close()
+        },
+        abort(error) {
+          controller.error(error)
+        },
+      }))
+    },
+  })
+
+  return new Response(stream)
+}
