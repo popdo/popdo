d40d1ab5e288831373028f693677a0ec021a6ddb
金 <30309735+popdo@users.noreply.github.com>
2023-03-11 13:19:34 +0800

    Update fetch.ts

--- src/pages/api/fetch.ts
+++ src/pages/api/fetch.ts
@@ -4,7 +4,7 @@ import { fetch, ProxyAgent } from 'undici'
 const httpsProxy = import.meta.env.HTTPS_PROXY
 const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
 
-const post:APIRoute = async (context:any) => {
+export const post:APIRoute = async (context:any) => {
   const options = await context.request.json()
   const {headers,body} = options
 
