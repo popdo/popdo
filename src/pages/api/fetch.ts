acaed08e8776fbdd2a81d5ef48d75db82e5679ca
金 <30309735+popdo@users.noreply.github.com>
2023-03-11 13:12:23 +0800

    Update fetch.ts

--- src/pages/api/fetch.ts
+++ src/pages/api/fetch.ts
@@ -1,4 +1,25 @@
-import { fetch } from 'undici'
-const post = async (options:any) => {
-  return await fetch('https://api.openai.com/v1/chat/completions', options)
+import type { APIRoute } from 'astro'
+import { fetch, ProxyAgent } from 'undici'
+
+const httpsProxy = import.meta.env.HTTPS_PROXY
+const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com').trim().replace(/\/$/,'')
+
+const post:APIRoute = async (context:any) => {
+  const options = await context.request.json()
+  const {headers,body} = options
+
+
+  const initOptions = {
+    headers,
+    method: 'POST',
+    body: JSON.stringify(body),
+  }
+  
+  if (httpsProxy) {
+    initOptions['dispatcher'] = new ProxyAgent(httpsProxy)
+  }
+  
+  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions) as Response
+  
+  return response
 }
