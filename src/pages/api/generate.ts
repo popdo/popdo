a630896c0ea2cce3c656dbd71b5a36af079d2a07
金 <30309735+popdo@users.noreply.github.com>
2023-03-12 00:27:05 +0800

    Update generate.ts

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -10,22 +10,15 @@ const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com'
 
 export const post: APIRoute = async (context) => {
   const options = await context.request.json()
-  const {headers,body} = options
 
-
-  const initOptions = {
-    headers,
-    method: 'POST',
-    body: JSON.stringify(body),
-  }
   // #vercel-disable-blocks
   if (httpsProxy) {
-    initOptions['dispatcher'] = new ProxyAgent(httpsProxy)
+    options['dispatcher'] = new ProxyAgent(httpsProxy)
   }
   // #vercel-end
 
   // @ts-ignore
-  const response = await fetch(`${baseUrl}/v1/chat/completions`, initOptions) as Response
+  const response = await fetch(`${baseUrl}/v1/chat/completions`, options) as Response
 
   return new Response(parseOpenAIStream(response))
 }
