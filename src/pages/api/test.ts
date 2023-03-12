f8af9439aafbd425c9118e29870781d6e9f9b36a
金 <30309735+popdo@users.noreply.github.com>
2023-03-12 17:39:28 +0800

    Create test.ts

---
+++ src/pages/api/test.ts
@@ -0,0 +1,27 @@
+import { fetch, ProxyAgent } from 'undici'
+
+export const fetchApi = async (options: any) => {
+  
+  const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+
+  if (!response.ok) await handleFetchError(response)
+
+  return response
+
+}
+
+
+const handleFetchError = async (response: Response) => {
+
+  const errorJson = await response.json()
+
+  let eMsg = errorJson.error?.message || errorJson.message || 'Unknown error'
+  if(response?.status === 400 && eMsg.startsWith("This model's maximum context length")){
+    eMsg = '可能当前话题聊太久了，请删除部分聊天内容再继续，或者发起新的话题！'
+  }
+  if(response?.status === 429 && eMsg.startsWith("Rate limit reached for")){
+    eMsg = '消息发送太快了，休息一下再发吧！'
+  }
+  throw new Error(`请求API错误-[${response.status}]: ${eMsg}`)
+
+}
